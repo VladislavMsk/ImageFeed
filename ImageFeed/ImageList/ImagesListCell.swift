24535eceb4e1c773fa67a346aeb0048b2e2a1952
Vladislav Tudos <vladislav.grand@gmail.com>
2023-12-23 12:51:14 +0300

    8 спринт. Версия: для отправки наставку в пачке и разбора ошибок.

---
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -0,0 +1,17 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Vladislav Tudos on 12.12.2023.
+//
+
+import UIKit
+
+final class ImagesListCell: UITableViewCell{
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dataLabel: UILabel!
+    
+    
+}
