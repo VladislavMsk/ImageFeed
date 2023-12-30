830cc5ec6a5e566c12931376de980fab5e9d7277
Vladislav Tudos <vladislav.grand@gmail.com>
2023-12-30 19:23:47 +0300

    Спринт 9, тема 3. Приложение крашится на 49 строке.

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,60 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Vladislav Tudos on 27.12.2023.
+//
+
+import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else {return}
+            SingleImage.image = image
+            rescaleAndCenterImageInScrollView(image: image)
+        }
+    }
+    
+    @IBOutlet private var SingleImage: UIImageView! //если убрать private, код работает
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction private func didTapBackButton() {
+           dismiss(animated: true, completion: nil)
+       }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.minimumZoomScale  = 0.1
+        scrollView.maximumZoomScale = 1.5
+        SingleImage.image = image
+        rescaleAndCenterImageInScrollView(image: image) //приложение крашится только после вызова функции здесь, если этот участок кода закоментировать - все ок
+        
+        
+    }
+}
+
+//MARK: - UIScrollViewDelegate
+extension SingleImageViewController:UIScrollViewDelegate{
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        SingleImage
+    }
+}
